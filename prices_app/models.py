25f296be46521a689366f8626d17faf3f9b3c2cd
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-08-01 21:48:23 +0330

    upload

--- prices_app/models.py
+++ prices_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Symbol(models.Model):
-    symbol = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=50, primary_key=True, )
 
     def __str__(self) -> str:
         return self.symbol
--- prices_app/views.py
+++ prices_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from typing import Tuple
 # Create your views here.
+
+def get_price(symbol: str) -> Tuple[str, int]:
+    pass
+
+
 
 def prices(request):
 
