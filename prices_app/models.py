b31892107af19bb425eea290b2a0012dba2b4abf
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-08-06 09:06:46 +0330

    upload

--- prices_app/models.py
+++ prices_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Symbol(models.Model):
-    symbol = models.CharField(max_length=50, primary_key=True, )
+    symbol = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return self.symbol
