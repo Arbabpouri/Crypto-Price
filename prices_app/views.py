4113f4d03a26883ed28422b658ce1b47896f7461
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2023-08-09 19:09:19 +0330

    upload

--- prices_app/views.py
+++ prices_app/views.py
@@ -12,7 +12,7 @@ class BinanceGetPriceResponse(BaseModel):
     code: Union[int, None] = None
     msg: Union[str, None] = None
     symbol: Union[str, None] = None
-    price: Union[float, None] = None
+    price: Union[str, None] = None
 
 
 class BinanceGetPriceChangePercentSend(BaseModel):
@@ -25,42 +25,45 @@ class BinanceGetPriceChangePercentResponse(BinanceGetPriceChangePercentSend):
     priceChangePercent: str = None
 
 
-def status_code_check(code):
-    if code == 200:
-        return True
-    return False
-
-
 class CoinTools:
 
     def __init__(self, symbol: str):
         self.symbol = symbol
 
-    def coin_icon(self) -> Union[str, None]:
+    @staticmethod
+    def __status_code_check(code):
+        if code == 200:
+            return True
+        return False
+
+    def coin_icon(self) -> Union[bytes, None, bool]:
         url = f"https://coinicons-api.vercel.app/api/icon/{self.symbol}"
         response = get(url=url)
-        if not status_code_check(response.status_code): return False
+        if not self.__status_code_check(response.status_code):
+            return False
         image_encoded = b64encode(response.content)
         return image_encoded
 
-    def coin_price(self) -> str:
+    def coin_price(self) -> Union[str, False]:
         url = f"https://api.binance.com/api/v3/ticker/price?symbol={self.symbol.upper()}USDT"
         response = get(url=url)
-        if not status_code_check(response.status_code): return False
-        response = BinanceGetPriceResponse(**response)
+        if not self.__status_code_check(response.status_code):
+            return False
+        response = BinanceGetPriceResponse(**response.json())
         if response.price is not None:
             return response.price
         return "Try agien"
 
-    def coin_price_change_percent(self):
+    def coin_price_change_percent(self) -> Union[str, bool, None]:
         url = "https://api.binance.com/api/v3/ticker/24hr"
-        parameters = BinanceGetPriceChangePercentSend(symbol=f"{self.symbol.upper()}USDT").dict()
+        parameters = BinanceGetPriceChangePercentSend(symbol=f"{self.symbol.upper()}USDT").model_dump(mode='json')
         response = post(url=url, json=parameters)
-        if not status_code_check(response.status_code): return False
+        if not self.__status_code_check(response.status_code):
+            return False
         response = BinanceGetPriceChangePercentResponse(**response.json())
         if response.priceChangePercent is not None:
             return response.priceChangePercent
-        return "Try agien"
+        return "Try again"
 
 
 def prices(request):
